fdf0c5e899ecdae101594905e6da2920ca448226
dominic-messenger-moj <dominic-messenger-moj@users.noreply.github.com>
2023-05-10 13:50:38 +0000

    updating project name after deployment from template repository

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -11,7 +11,7 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'HMPPS Typescript Template'
+  app.locals.applicationName = 'Digital Prison Reporting Mi Ui'
 
   // Cachebusting version string
   if (production) {
