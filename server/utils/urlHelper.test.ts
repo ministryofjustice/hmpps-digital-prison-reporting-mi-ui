02605795bb5c18df4d1eaa8bb6a2069b468c59da
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2023-07-14 13:23:44 +0100

    [DPR-576] Fix for filters not staying when changing page (#42)

---
+++ server/utils/urlHelper.test.ts
@@ -0,0 +1,63 @@
+import UrlHelper from './urlHelper'
+import { ReportQuery } from '../types/reports/class'
+import Dict = NodeJS.Dict
+
+const filtersPrefix = 'filters.'
+
+describe('Create URL', () => {
+  it('Clear all filters', () => {
+    const currentQueryParams: ReportQuery = {
+      selectedPage: 10,
+      pageSize: 20,
+      sortColumn: '30',
+      sortedAsc: false,
+      filters: {
+        direction: 'out',
+      },
+    }
+    const updateQueryParams: Dict<string> = null
+
+    const url = UrlHelper.createUrlForParameters(currentQueryParams, updateQueryParams, filtersPrefix)
+
+    expect(url).toEqual('?selectedPage=10&pageSize=20&sortColumn=30&sortedAsc=false')
+  })
+
+  it('Clear single filter', () => {
+    const currentQueryParams: ReportQuery = {
+      selectedPage: 10,
+      pageSize: 20,
+      sortColumn: '30',
+      sortedAsc: false,
+      filters: {
+        direction: 'out',
+        type: 'jaunt',
+      },
+    }
+    const updateQueryParams: Dict<string> = {
+      'filters.direction': '',
+    }
+
+    const url = UrlHelper.createUrlForParameters(currentQueryParams, updateQueryParams, filtersPrefix)
+
+    expect(url).toEqual('?selectedPage=10&pageSize=20&sortColumn=30&sortedAsc=false&filters.type=jaunt')
+  })
+
+  it('Change page with filters', () => {
+    const currentQueryParams: ReportQuery = {
+      selectedPage: 10,
+      pageSize: 20,
+      sortColumn: '30',
+      sortedAsc: false,
+      filters: {
+        direction: 'out',
+      },
+    }
+    const updateQueryParams: Dict<string> = {
+      selectedPage: '11',
+    }
+
+    const url = UrlHelper.createUrlForParameters(currentQueryParams, updateQueryParams, filtersPrefix)
+
+    expect(url).toEqual('?selectedPage=11&pageSize=20&sortColumn=30&sortedAsc=false&filters.direction=out')
+  })
+})
--- server/utils/urlHelper.ts
+++ server/utils/urlHelper.ts
@@ -1,9 +1,13 @@
 import querystring from 'querystring'
 import Dict = NodeJS.Dict
-import { ReportQuery, toRecord } from '../types/reports/class'
+import { ReportQuery, toRecord, toRecordWithFilterPrefix } from '../types/reports/class'
 
-const createUrlForParameters = (currentQueryParams: ReportQuery, updateQueryParams: Dict<string>) => {
-  let queryParams: Dict<string> = toRecord(currentQueryParams)
+const createUrlForParameters = (
+  currentQueryParams: ReportQuery,
+  updateQueryParams: Dict<string>,
+  filtersPrefix: string,
+) => {
+  let queryParams: Dict<string> = toRecordWithFilterPrefix(currentQueryParams, filtersPrefix)
 
   if (updateQueryParams) {
     Object.keys(updateQueryParams).forEach(q => {
@@ -38,7 +42,8 @@ const createUrlForParameters = (currentQueryParams: ReportQuery, updateQueryPara
 export default {
   createUrlForParameters,
 
-  getCreateUrlForParametersFunction: (currentQueryParams: ReportQuery) => {
-    return (updateQueryParams: Dict<string>) => createUrlForParameters(currentQueryParams, updateQueryParams)
+  getCreateUrlForParametersFunction: (currentQueryParams: ReportQuery, filtersPrefix: string) => {
+    return (updateQueryParams: Dict<string>) =>
+      createUrlForParameters(currentQueryParams, updateQueryParams, filtersPrefix)
   },
 }
