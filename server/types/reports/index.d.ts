9b7fa6af54b202943264a33fbf748f60d228db05
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-09-13 17:05:52 +0100

    [DPR2-68] Extract report list as component

--- server/types/reports/index.d.ts
+++ server/types/reports/index.d.ts
@@ -10,3 +10,24 @@ export interface ListRequest {
 export interface FilteredListRequest extends ListRequest {
   filters: Dict<string>
 }
+
+export interface FieldDefinition {
+  name: string
+  displayName: string
+  wordWrap?: 'None'
+  filter?: FilterDefinition
+  sortable: boolean
+  defaultSortColumn: boolean
+  type: 'String' | 'Date' | 'Long'
+}
+
+export interface FilterDefinition {
+  type: 'Radio' | 'Select' | 'DateRange'
+  staticOptions?: FilterOption[]
+  defaultValue?: string
+}
+
+export interface FilterOption {
+  name: string
+  displayName: string
+}
