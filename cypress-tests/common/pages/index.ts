abe6d791931c0ab66cefafe55c79d3bb99653da1
rossdowthwaite <rossdowthwaite@gmail.com>
2024-08-28 16:56:39 +0100

    fix integration test

--- cypress-tests/common/pages/index.ts
+++ cypress-tests/common/pages/index.ts
@@ -2,7 +2,7 @@ import Page, { PageElement } from './page'
 
 export default class IndexPage extends Page {
   constructor() {
-    super('Home')
+    super('Digital Prison Reporting')
   }
 
   headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')
