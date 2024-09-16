57c3e3786e67bd21e7bb763af502f14d26b0300c
rossdowthwaite <rossdowthwaite@gmail.com>
2024-09-16 16:02:23 +0100

    integrated dahsboards

--- server/data/index.ts
+++ server/data/index.ts
@@ -9,6 +9,8 @@ initialiseAppInsights()
 buildAppInsightsClient()
 
 import ReportingClient from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/data/reportingClient'
+import MetricsClient from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/data/metricsClient'
+import DashboardClient from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/data/dashboardClient'
 import UserDataStore from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/data/userDataStore'
 import HmppsAuthClient from './hmppsAuthClient'
 import config from '../config'
@@ -20,6 +22,8 @@ export const dataAccess = () => ({
   hmppsAuthClient: new HmppsAuthClient(),
   hmppsManageUsersClient: new HmppsManageUsersClient(),
   reportingClient: new ReportingClient(config.apis.reporting),
+  metricsClient: new MetricsClient(config.apis.reporting),
+  dashboardClient: new DashboardClient(config.apis.reporting),
   userClient: new UserClient(config.apis.reporting),
   userDataStore: new UserDataStore(createRedisClient()),
 })
