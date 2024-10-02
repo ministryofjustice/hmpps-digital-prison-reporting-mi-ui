67ed09aceb222bf41a5c852ebc92b81fd692acc0
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-10-02 11:36:10 +0100

    [DPR2-1245] Fix maintenance screen redirect when user is not logged in

--- server/middleware/siteMaintenanceRedirect.ts
+++ server/middleware/siteMaintenanceRedirect.ts
@@ -1,12 +1,22 @@
 import type { RequestHandler } from 'express'
 import config from '../config'
 
+export const pathFragmentsToNotRedirect = [
+  '/info',
+  '/health',
+  '/ping',
+  '/maintenance',
+  'sign-in',
+  '/sign-out',
+  'assets',
+]
+
 export default function siteMaintenanceRedirect(): RequestHandler {
   return (req, res, next) => {
     const inMaintenanceMode = config.maintenanceMode
-    const onMaintenancePage = req.path === '/maintenance' || req.path.includes('assets')
+    const doNotRedirect = pathFragmentsToNotRedirect.some(f => req.path.includes(f))
 
-    if (inMaintenanceMode && !onMaintenancePage) {
+    if (inMaintenanceMode && !doNotRedirect) {
       return res.redirect('/maintenance')
     }
 
