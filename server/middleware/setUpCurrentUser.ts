b917453c3bf7d6d27506dca6793660f995605606
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-11-05 16:28:23 +0000

    Dpr2 1168 fe lib bump (#577)
    3.31.0

--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -4,12 +4,7 @@ import tokenVerifier from '../data/tokenVerification'
 import populateCurrentUser from './populateCurrentUser'
 import type { Services } from '../services'
 
-export default function setUpCurrentUser({
-  userService,
-  requestedReportService,
-  recentlyViewedService,
-  bookmarkService,
-}: Services): Router {
+export default function setUpCurrentUser(services: Services): Router {
   const router = Router({ mergeParams: true })
 
   router.get('/roleError', (req, res) => {
@@ -18,6 +13,6 @@ export default function setUpCurrentUser({
   })
 
   router.use(auth.authenticationMiddleware(tokenVerifier))
-  router.use(populateCurrentUser(userService, requestedReportService, recentlyViewedService, bookmarkService))
+  router.use(populateCurrentUser(services))
   return router
 }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -6,9 +6,6 @@ import addDashboardRoutes from '@ministryofjustice/hmpps-digital-prison-reportin
 import addDownloadRoutes from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/routes/download'
 import addRecentlyViewedRoutes from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/routes/recentlyViewed'
 import UserReportsListUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/user-reports/utils'
-import RecentlyViewedUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/user-reports-viewed-list/utils'
-import RequestedReportsUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/user-reports-request-list/utils'
-import BookmarklistUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/user-reports-bookmarks-list/utils'
 import ReportslistUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/reports-list/utils'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
@@ -31,37 +28,10 @@ export default function routes(services: Services): Router {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', async (req, res) => {
-    const requestedReportsData = await UserReportsListUtils.renderList({
-      res,
-      storeService: services.requestedReportService,
-      maxRows: 6,
-      filterFunction: RequestedReportsUtils.filterReports,
-      type: 'requested',
-    })
-
-    const recentlyViewedData = await UserReportsListUtils.renderList({
-      res,
-      storeService: services.recentlyViewedService,
-      maxRows: 6,
-      filterFunction: RecentlyViewedUtils.filterReports,
-      type: 'requested',
-    })
-
-    const bookmarks = await BookmarklistUtils.renderBookmarkList({
-      res,
-      req,
-      services,
-      maxRows: 6,
-    })
-
-    const reportsData = await ReportslistUtils.mapReportsList(res, services)
-
     res.render('pages/home', {
       title: 'Digital Prison Reporting',
-      requestedReports: requestedReportsData,
-      viewedReports: recentlyViewedData,
-      bookmarks,
-      reports: reportsData,
+      ...(await UserReportsListUtils.initLists({ res, req, services })),
+      reports: await ReportslistUtils.mapReportsList(res, services),
     })
   })
 
