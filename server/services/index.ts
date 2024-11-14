96deb6fa823b60b53a6f41e18113d2bbae787cb0
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-11-14 13:47:35 +0000

    Dpr2 1468 content review changes (#591)
    - content review changes
    - Bump FE ob to 3.32.0

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,20 +1,17 @@
 import ReportingService from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/services/reportingService'
 import { Services as dprServices } from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/types/Services'
 import DashboardService from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/services/dashboardService'
-import MetricService from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/services/metricsService'
 import { createUserStoreServices } from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/utils/StoreServiceUtils'
 import { dataAccess } from '../data'
 import UserService from './userService'
 import HmppsComponentsService from './hmppsComponentsService'
 
 export const services = (): Services => {
-  const { reportingClient, userClient, hmppsManageUsersClient, userDataStore, dashboardClient, metricsClient } =
-    dataAccess()
+  const { reportingClient, userClient, hmppsManageUsersClient, userDataStore, dashboardClient } = dataAccess()
 
   const userService = new UserService(hmppsManageUsersClient, userClient)
   const reportingService = new ReportingService(reportingClient)
   const hmppsComponentsService = new HmppsComponentsService()
-  const metricService = new MetricService(metricsClient)
   const dashboardService = new DashboardService(dashboardClient)
   const userStoreServices = createUserStoreServices(userDataStore)
 
@@ -22,7 +19,6 @@ export const services = (): Services => {
     userService,
     reportingService,
     hmppsComponentsService,
-    metricService,
     dashboardService,
     ...userStoreServices,
   }
