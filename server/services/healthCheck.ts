cdf178e002a790929e6465990864669e619c6d8e
rossdowthwaite <rossdowthwaite@gmail.com>
2025-04-07 09:55:37 +0100

    linting

--- server/services/healthCheck.ts
+++ server/services/healthCheck.ts
@@ -44,8 +44,9 @@ function addAppInfo(result: HealthCheckResult): HealthCheckResult {
 
 function getBuild() {
   try {
-    // eslint-disable-next-line import/no-unresolved,global-require,@typescript-eslint/no-require-imports
-    return require('../../build-info.json')
+    // eslint-disable-next-line
+    const buildInfo = require('../../build-info.json')
+    return buildInfo
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (ex) {
     return null
