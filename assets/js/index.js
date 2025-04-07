dfb9797b5e8512c6127d68a3c4c77028704dffc0
rossdowthwaite <rossdowthwaite@gmail.com>
2025-04-07 09:41:19 +0100

    linting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
-import * as dprFrontend from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/all.mjs'
+import * as dprFrontend from '@ministryofjustice/hmpps-digital-prison-reporting-frontend'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
