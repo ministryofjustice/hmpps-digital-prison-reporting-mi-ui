0274d8ce9fef0c0ecc8c33c4b0cabd460c1a9e06
rossdowthwaite <rossdowthwaite@gmail.com>
2025-05-22 13:52:56 +0100

    remove staff ID

--- server/config.ts
+++ server/config.ts
@@ -43,7 +43,6 @@ const apiCommonConfig = {
 
 const automaticBookmarkConfig = {
   caseloads: {},
-  staffIds: <string[]>[],
 }
 
 export default {
--- server/middleware/setUpBookmarks.ts
+++ server/middleware/setUpBookmarks.ts
@@ -8,21 +8,19 @@ export default function setUpBookmarks(services: Services): RequestHandler {
   return async (req, res, next) => {
     try {
       if (res.locals.user && !req.session.bookmarksInitialised) {
-        const { uuid, activeCaseLoadId, staffId } = res.locals.user
+        const { uuid, activeCaseLoadId } = res.locals.user
         const { automaticBookmarkConfig } = config.dpr
 
-        if (automaticBookmarkConfig.staffIds.includes(staffId)) {
-          logger.info(` Initialising bookmarks for user: ${res.locals.user && res.locals.user.username}`)
+        logger.info(` Initialising bookmarks for user: ${res.locals.user && res.locals.user.username}`)
 
-          await BookmarkUtils.preBookmarkReportsByRoleId(
-            uuid,
-            activeCaseLoadId,
-            services,
-            automaticBookmarkConfig.caseloads,
-          )
-        }
-        req.session.bookmarksInitialised = true
+        await BookmarkUtils.preBookmarkReportsByRoleId(
+          uuid,
+          activeCaseLoadId,
+          services,
+          automaticBookmarkConfig.caseloads,
+        )
       }
+      req.session.bookmarksInitialised = true
       return next()
     } catch (error) {
       logger.error(error, `Failed to initialise bookmarks : ${res.locals.user && res.locals.user.username}`)
