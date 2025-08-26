680b3dd7d782c80a97e8af5d070fd20be352b809
rossdowthwaite <rossdowthwaite@gmail.com>
2025-08-26 10:14:09 +0100

    dprUser in populateCurrentUser

--- server/middleware/populateCurrentUser.ts
+++ server/middleware/populateCurrentUser.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from 'express'
+import DprUser from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/types/DprUser'
 import logger from '../../logger'
 import { Services } from '../services'
 
@@ -7,9 +8,17 @@ export default function populateCurrentUser(services: Services): RequestHandler 
     try {
       if (res.locals.user) {
         const user = await services.userService.getUser(res.locals.user.token)
+
         if (user) {
+          const dprUser = new DprUser()
+          dprUser.token = res.locals.user.token
+          dprUser.id = user.uuid
+          dprUser.activeCaseLoadId = user.activeCaseLoadId
+          dprUser.emailAddress = user.email
+          dprUser.displayName = user.displayName
           res.locals.user = { ...user, ...res.locals.user }
           req.session.userDetails = res.locals.user
+          res.locals.dprUser = dprUser
         } else {
           logger.info('No user available')
         }
