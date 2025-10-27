f18d6f8b79e076123abf242d3a2ab168651c6f4e
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2025-10-27 11:55:51 +0000

    fix sentry and appinsights (#846)
    Co-authored-by: James Reed <james.reed1@justice.gov.uk>

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -20,7 +20,10 @@ export default function setUpWebSecurity(): Router {
   // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
   // This ensures only scripts we trust are loaded, and not anything injected into the
   // page by an attacker.
-  const scriptSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
+  const scriptSrc = [
+    "'self' https://browser.sentry-cdn.com https://js.sentry-cdn.com",
+    (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+  ]
   const styleSrc = [
     "'self' https://browser.sentry-cdn.com https://js.sentry-cdn.com",
     (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
