03e971a8c55a20772c74832de0c4cccced572aa5
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2026-01-05 11:11:48 +0000

    Update deps security (#883)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: James Reed <james.reed1@justice.gov.uk>

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,12 +4,12 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     'node_modules/@parcel/watcher@2.5.1': 'FORBID',
-    'node_modules/cypress@13.17.0': 'ALLOW',
+    'node_modules/cypress@14.5.4': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
     'node_modules/unrs-resolver@1.11.1': 'FORBID',
     'node_modules/esbuild@0.25.10': 'ALLOW',
     'node_modules/@sentry/cli@2.56.1': 'ALLOW',
-    'node_modules/@badeball/cypress-cucumber-preprocessor@21.0.3': 'FORBID',
+    'node_modules/@badeball/cypress-cucumber-preprocessor@22.2.0': 'FORBID',
   },
 })
