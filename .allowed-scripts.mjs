57a9e58c8da37d415bd11fdde82282d291295464
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2026-02-13 14:15:51 +0000

    update allowed-scripts

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -7,7 +7,7 @@ export default configureAllowedScripts({
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
     'node_modules/unrs-resolver@1.11.1': 'FORBID',
-    'node_modules/esbuild@0.27.2': 'ALLOW',
+    'node_modules/esbuild@0.27.3': 'ALLOW',
     'node_modules/@sentry/cli@3.1.0': 'ALLOW',
   },
 })
