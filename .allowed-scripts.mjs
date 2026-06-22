4faad7e6e471bb7ca2359b2f65929d6ce8717e13
rossdowthwaite <rossdowthwaite@gmail.com>
2026-06-22 09:43:13 +0100

    allow script update

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,10 +2,12 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
+    'node_modules/@grpc/proto-loader/node_modules/protobufjs@7.6.4': 'FORBID',
     'node_modules/@parcel/watcher@2.5.1': 'FORBID',
     'node_modules/cypress@15.17.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
+    'node_modules/protobufjs@8.0.1': 'FORBID',
     'node_modules/unrs-resolver@1.11.1': 'FORBID',
     'node_modules/esbuild@0.28.1': 'ALLOW',
     'node_modules/@sentry/cli@3.5.0': 'ALLOW',
